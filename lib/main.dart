db63a0db0a8c88131bf3ecb978a6a78983d9e952
Mackley Carmona <mackleycarmonaalvarez@gmail.com>
2025-02-06 13:25:12 -0500

    Set up Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:telepatia_ai/config/config.dart';
+import 'package:telepatia_ai/firebase_options.dart';
 
-void main() => runApp(const ProviderScope(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  return runApp(
+    const ProviderScope(
       child: SafeArea(child: MyApp()),
-    ));
+    )
+  );
+}
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
